3295ded1d5acafacedb51d298a63e21b0e531079
Essien Emmanuel <essienemma300@gmail.com>
2025-08-04 17:41:18 +0100

    modified screen manager class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import Engine from "@/core/engine";
 import TitleScreen from "./screens/title";
 import Prologue from "./screens/prologue";
+import GameScreen from "./screens/game";
 
 const engine = new Engine();
-engine.screenManager.register({ name: "title", screen: TitleScreen });
-engine.screenManager.register({ name: "prologue", screen: Prologue });
+engine.screenManager
+  .register({ name: "title", screen: TitleScreen })
+  .register({ name: "prologue", screen: Prologue })
+  .register({ name: "game", screen: GameScreen });
+
 engine.start("title");
