df1cd6bd21276871079e8e5a7bfc55d43947ea2d
Essien Emmanuel <essienemma300@gmail.com>
2025-08-19 16:19:31 +0100

    added blessed as renderer

--- src/render/index.ts
+++ src/render/index.ts
@@ -1,1 +1,11 @@
-class Renderer {}
+import blessed, { Widgets } from "blessed";
+
+export class Renderer {
+  createScreen(options?: Widgets.IScreenOptions) {
+    return blessed.screen(options);
+  }
+
+  createBox(options?: Widgets.BoxOptions) {
+    return blessed.box(options);
+  }
+}
