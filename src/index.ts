0743415c05aec49f4be6f239423ee70c835a8df9
Essien Emmanuel <essienemma300@gmail.com>
2025-08-27 05:28:52 +0100

    implemented show type result

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { applyTextStyle } from "./core/utils";
 import { styleFont, styleFontReset } from "./renderer/font";
 
 export function checkSpace(keypress: string) {
-  console.log(keypress);
   if (keypress === "\u0020") {
     console.log("is space bar");
   }
@@ -19,7 +18,6 @@ function matchKeypressToTextPromt(
   keypress: string,
   keypressCount: number
 ) {
-  checkSpace(keypress);
   if (textPrompt[keypressCount - 1] !== keypress) {
     return { match: false, fontPos: keypressCount, mistake: true };
   }
@@ -45,19 +43,52 @@ export function updateStyledTextPrompt({
   return styledKeys.join(`\x1b`) + styleFontReset;
 }
 
-const progressStats = {
-  speed: 0,
-  accuracy: 0,
-  time: 0,
-  alignedSpeed: 0,
+export function calculateWpm(
+  keypressCount: number,
+  uncorrectedErrorsCount: number,
+  elapsedTime: number
+) {
+  const grossWpm = keypressCount / 5;
+  const oneMin = 1000; //* 60;
+  const elapsedTimeInMin = elapsedTime / oneMin;
+
+  const wpm = grossWpm - uncorrectedErrorsCount / elapsedTimeInMin;
+  return Math.round(wpm);
+}
+
+export function calculateAccuracy(
+  noOfCorrectChar: number,
+  totalPromptLength: number
+) {
+  return (noOfCorrectChar / totalPromptLength) * 100;
+}
+export type PlayerStat = {
+  wpm: number;
+  accuracy: number;
+  timeout: number;
+  mistakes: number;
 };
 
+export function showStats(playerStat: PlayerStat) {
+  const { wpm, accuracy, timeout, mistakes } = playerStat;
+  const timeoutInmin = timeout / 1000;
+  console.log("Result");
+  console.log("speed: " + wpm + "wpm");
+  console.log("accuracy: " + Math.round(accuracy) + "%");
+  console.log("time: " + Math.round(timeoutInmin) + "s");
+  console.log("mistakes: " + mistakes);
+}
+
 function $$game() {
-  const textPrompt = "Attack the king";
+  // const textPrompt = "Attack the king";
+  const textPrompt =
+    "To keep the width at 300px, no matter the amount of padding, you can use the box-sizing property This causes the element to maintain its actual width; if you increase the padding, the available content space will decrease.";
 
   const textPromptLength = textPrompt.length;
+  const timeout = 1000 * 30;
 
   let mistakes = 0;
+  let noOfcorrectChar = 0;
 
   let styledTextPrompt = applyTextStyle({
     text: textPrompt,
@@ -66,14 +97,20 @@ function $$game() {
   });
   write(styledTextPrompt + styleFontReset);
 
+  const prevTime = Date.now();
+
   hideCursor();
 
   handleKeypress(
     ({ storedKeypress, keypress, keypressCount, isTimeout }) => {
+      const currentTime = Date.now();
+      const elapsedTime = currentTime - prevTime;
+
       if (isTimeout) {
-        console.log("timeout");
-        console.log("Typing Result");
-        console.log("mistakes: ", mistakes);
+        const accuracy = calculateAccuracy(noOfcorrectChar, textPromptLength);
+        const wpm = calculateWpm(keypressCount, mistakes, elapsedTime);
+
+        showStats({ accuracy, timeout: elapsedTime, mistakes, wpm });
         showCursor();
         process.exit();
       }
@@ -94,23 +131,27 @@ function $$game() {
 
       styledTextPrompt = updatedTextPrompt;
 
-      if (mistake) ++mistakes;
+      if (mistake) {
+        ++mistakes;
+      } else {
+        ++noOfcorrectChar;
+      }
 
       if (keypressCount === textPromptLength) {
         if (storedKeypress === textPrompt) {
           console.log("\nyou win");
-          console.log("mistakes: ", mistakes);
-          showCursor();
-          process.exit();
         } else {
           console.log("\nCompleted");
-          console.log("mistakes: ", mistakes);
-          showCursor();
-          process.exit();
         }
+        const accuracy = calculateAccuracy(noOfcorrectChar, textPromptLength);
+        const wpm = calculateWpm(keypressCount, mistakes, elapsedTime);
+
+        showStats({ accuracy, timeout: elapsedTime, mistakes, wpm });
+        showCursor();
+        process.exit();
       }
     },
-    { storeKeypress: true, resetWindow: true }
+    { storeKeypress: true, resetWindow: true, timeout }
   );
 }
 
